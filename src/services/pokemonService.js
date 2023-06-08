7af6f6fae56f61c0e94b17ad609695f5a10ca174
FranSar5868 <fsarrias@coit.es>
2023-06-08 23:12:09 +0200

    I use set to avoid fetching duplcate pokemons

--- src/services/pokemonService.js
+++ src/services/pokemonService.js
@@ -12,14 +12,29 @@ const pokeAPI = axios.create({
 export default {
   getRandomPokemons(numPokemons) {
     const maxNumOfPokemons = 1010
-    const pokemonIds = Array.from({ length: numPokemons }, () =>
-      Math.floor(Math.random() * maxNumOfPokemons) + 1
-    )
-    const pokemonRequests = pokemonIds.map(id =>
+    const pokemonIds = new Set()
+
+    while (pokemonIds.size < numPokemons) {
+      const randomId = Math.floor(Math.random() * maxNumOfPokemons) + 1
+      pokemonIds.add(randomId)
+    }
+
+    const pokemonRequests = Array.from(pokemonIds).map(id =>
       pokeAPI.get(`/pokemon/${id}`).then(response => response.data)
     )
+
     return Promise.all(pokemonRequests)
   },
+  // getRandomPokemons(numPokemons) {
+  //   const maxNumOfPokemons = 1010
+  //   const pokemonIds = Array.from({ length: numPokemons }, () =>
+  //     Math.floor(Math.random() * maxNumOfPokemons) + 1
+  //   )
+  //   const pokemonRequests = pokemonIds.map(id =>
+  //     pokeAPI.get(`/pokemon/${id}`).then(response => response.data)
+  //   )
+  //   return Promise.all(pokemonRequests)
+  // },
   getSinglePokemon(pokemonId) {
     return pokeAPI.get(`/pokemon/${pokemonId}`);
   }
