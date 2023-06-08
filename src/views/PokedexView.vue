0b43489818ad62d9b06567fd9dd4041d59faba58
fsarrias <fsarrias@kolab.es>
2023-06-08 12:28:53 +0200

    I add the filter to cards

--- src/views/PokedexView.vue
+++ src/views/PokedexView.vue
@@ -1,27 +1,31 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 
 import Card from '../components/Card.vue'
 import pokemonService from '../services/pokemonService.js'
 
 const cards = ref([])
+const search = ref("")
 
 onMounted(async () => {
   cards.value = await pokemonService.getRandomPokemons(10)
 })
+
+const filteredCards = computed(() => {
+  return cards.value.filter((card) => {
+    return card.name.toLowerCase().includes(search.value.toLowerCase())
+  })
+})
+
 </script>
 
 <template>
   <div class="new">
     <h1>Pokedex</h1>
-
+    <input v-model="search" type="text" placeholder="type to search...">
+    <br>
     <section class="cards">
-      <!-- <div v-for="card in cards" :key="card.id"> -->
-        <!-- <div v-for="card in cards" :key="card.id"></div> -->
-        <!-- Display the Pokémon card information -->
-        <!-- <pre>{{ card }}</pre> -->
-        <Card v-for="card in cards" :key="card.id" :info="card"/>
-      <!-- </div> -->
+        <Card v-for="card in filteredCards" :key="card.id" :info="card"
     </section>
   </div>
 </template>
@@ -29,5 +33,11 @@ onMounted(async () => {
 <style>
 .new {
   display: grid;
+}
+.cards {
+  display: flex;
+  gap: 2rem;
+  max-width: 50vw;
+  flex-wrap: wrap;
 }
 </style>
