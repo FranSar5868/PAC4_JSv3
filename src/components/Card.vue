dd26c07eb1979724c9479c36edaab4767d65937c
fsarrias <fsarrias@kolab.es>
2023-06-08 12:48:03 +0200

    Minor CSS changes to the card view

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -8,10 +8,12 @@ const props = defineProps(['info'])
 
 <template>
   <article class="card">
-    I'm a Pokemon!
-    <h2>{{ props.info.name }}</h2>
-    <img :src="props.info.thumbnailUrl" alt="">
-    <button @click="$router.push({ name: 'photo', params: {photoId: props.info.id } })">View more...</button>
+    <!-- I'm a Pokemon! -->
+    <h2 class="name">{{ props.info.name }}</h2>
+    <img :src="props.info.sprites.front_default" alt="props.info.name">
+    <h3 class="data">Atac: {{ props.info.stats[1].base_stat }}</h3>
+    <h3 class="data">Defensa: {{ props.info.stats[1].base_stat }}</h3>
+    <button @click="$router.push({ name: 'photo', params: {photoId: props.info.id } })">Detalls</button>
   </article>
 </template>
 
@@ -27,6 +29,15 @@ const props = defineProps(['info'])
   width: fit-content;
   min-width: 200px;
   max-width: 250px;
+}
+.name {
+  text-align: center;
+  text-transform: uppercase;
+  font-weight: bold;
+}
+
+.data {
+  text-align: center;
 }
 .card button {
   margin-top: auto;
