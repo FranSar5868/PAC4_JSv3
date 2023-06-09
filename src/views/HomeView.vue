c7533de7f57d7c6d139224897a98d6684cb2c4aa
FranSar5868 <fsarrias@coit.es>
2023-06-10 01:28:44 +0200

    MInor CSS in Home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,14 +8,14 @@
     <div class="image-wrapper">
     <router-link to="/pokedex">
       <div class="image-container">
-      <img src="../assets/pokeball.png" alt="Image 1">
+      <img src="../assets/pokeball.png" alt="Pokedex">
       <span class="image-text">Go to the Pokedex...</span>
     </div>
     </router-link>
 
     <router-link to="/battle">
       <div class="image-container">
-      <img src="../assets/pokeball.png" alt="Image 2">
+      <img src="../assets/pokeball.png" alt="Pokemon Battle">
       <span class="image-text">Go to the Battle of Pokemons...</span>
     </div>
     </router-link>
@@ -36,5 +36,11 @@
   color: var (--text-color);
   margin: 0 0.5rem;
 }
-
+.image-text {
+  margin-top: 0.5rem;
+  text-transform: uppercase;
+}
+.image{
+  width: 100px;
+}
 </style>
