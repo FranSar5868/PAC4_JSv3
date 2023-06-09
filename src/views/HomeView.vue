222327caffc8c83b01d5234da35bb494f36fb43f
FranSar5868 <fsarrias@coit.es>
2023-06-10 01:11:55 +0200

    I change the home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,40 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+    <div class="image-wrapper">
+    <router-link to="/pokedex">
+      <div class="image-container">
+      <img src="../assets/pokeball.png" alt="Image 1">
+      <span class="image-text">Go to the Pokedex...</span>
+    </div>
+    </router-link>
+
+    <router-link to="/battle">
+      <div class="image-container">
+      <img src="../assets/pokeball.png" alt="Image 2">
+      <span class="image-text">Go to the Battle of Pokemons...</span>
+    </div>
+    </router-link>
+  </div>
   </main>
 </template>
+
+<style scoped>
+   .image-wrapper {
+    display: flex;
+    justify-content: center;
+   }
+
+.image-container {
+  text-align: center;
+  padding: 1rem;
+  background-color: var (--bg-color);
+  color: var (--text-color);
+  margin: 0 0.5rem;
+}
+
+</style>
